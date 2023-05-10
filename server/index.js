9b145dd96e4a936f054b5a37c0e41792b7188fd2
Sergey <shymkosergey@gmail.com>
2023-05-10 18:28:52 +0200

    second cors fix

--- server/index.js
+++ server/index.js
@@ -22,9 +22,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors({ origin: ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'], credentials: true }))
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
